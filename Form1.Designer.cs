27160cbd9002c9547942b20bd9ca884b42f8bb90
morales.jose <morales.jose@icave.com.mx>
2024-06-12 08:37:54 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace MasivePrinter {
+    partial class Form1 {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Path = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.AllowDrop = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Path});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 34);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(710, 234);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listView1_DragDrop);
+            this.listView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listView1_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Arrastra al cuadro los PDF a imprimir:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(647, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Imprimir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(12, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Borrar lista";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Path
+            // 
+            this.Path.Text = "Archivo";
+            this.Path.Width = 720;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 296);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(475, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Impresora:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::MasivePrinter.Properties.Resources.logo;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(672, 274);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 361);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Impresión Masiva";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColumnHeader Path;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace MasivePrinter {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            GetPrinters();
+        }
+
+        private void listView1_DragEnter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void listView1_DragDrop(object sender, DragEventArgs e) {
+            //lista de path de los archivos arrastrados
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            //Agregar a la lista del listview
+            foreach (string file in files) {
+                if (file.ToLower().Contains(".pdf")) {
+                    listView1.Items.Add(file);
+                    //string filename = file.Split('\\')[file.Split('\\').Count() - 1];
+                    //listView1.Items.Add(filename);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            listView1.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (comboBox1.SelectedIndex >= 0) {
+
+                //Imprimir
+                foreach (ListViewItem ruta in listView1.Items) {
+                    string comando = AppDomain.CurrentDomain.BaseDirectory + "sumatra.exe \"" + ruta.Text + "\" -print-to \"" + comboBox1.SelectedItem.ToString() + "\" -silent -exit-when-done";
+                    Console.WriteLine(comando + "\r\n");
+
+                    var proc1 = new ProcessStartInfo();
+                    string anyCommand;
+                    proc1.UseShellExecute = true;
+                    //proc1.WorkingDirectory = @"C:\Windows\System32";
+
+                    proc1.FileName = AppDomain.CurrentDomain.BaseDirectory + "\\Resources\\sumatra.exe";
+                    //proc1.Verb = "runas";
+                    proc1.Arguments = " \"" + ruta.Text + "\" -print-to \"" + comboBox1.SelectedItem.ToString() + "\" -silent -exit-when-done";
+                    proc1.WindowStyle = ProcessWindowStyle.Hidden;
+                    Process.Start(proc1);
+                }
+
+            } else {
+                MessageBox.Show("Selecciona una impresora");
+            }
+            
+            
+        }
+
+        private void GetPrinters() {
+            comboBox1.Items.Clear();
+            foreach (string printer in System.Drawing.Printing.PrinterSettings.InstalledPrinters) {
+                comboBox1.Items.Add(printer);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) {
+            MessageBox.Show("♥︎");
+        }
+    }
+}
